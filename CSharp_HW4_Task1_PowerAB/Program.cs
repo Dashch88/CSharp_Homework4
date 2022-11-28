5d8161ce57f36cc610c60b3ca29770086f7c48da
Dashch <dashch88@gmail.com>
2022-11-28 21:16:05 +0400

    Added Task 1. A to the Power of B.

---
+++ CSharp_HW4_Task1_PowerAB/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Не используя Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int PowAB(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+Console.Clear();
+
+Console.Write("Введите целое число A: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите целое положительное число B: ");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (numB < 0)
+{
+    Console.WriteLine("Введена не натуральная степень B.");
+}
+else
+{
+    Console.WriteLine("Возведение числа A в степень B равно: " + PowAB(numA, numB));
+}
