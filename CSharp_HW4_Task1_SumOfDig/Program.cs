70661f18b8606d8eacb6d08737d8b2b6c2cded09
Dashch <dashch88@gmail.com>
2022-11-28 22:01:21 +0400

    Added Task 2. Sum Of Digits

---
+++ CSharp_HW4_Task1_SumOfDig/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int sumOfDig = 0;
+
+Console.WriteLine();
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i] != '-' && str[i] != '.' && str[i] != ',')
+    {
+        sumOfDig = sumOfDig + int.Parse(str[i].ToString());
+    }
+}
+
+Console.Write("Сумма всех цифр в введённом числе равна: " + sumOfDig);
+Console.WriteLine();
